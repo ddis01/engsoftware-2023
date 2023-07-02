80f14e6a9fd1f65c0b04be14e7399b5ba856dfa0
Darline <131263833+ddis01@users.noreply.github.com>
2023-07-02 16:29:41 -0300

    Update crud.test.js
    Cod. do Trabalho

--- 2o-bimestre/bimestral/test/crud.test.js
+++ 2o-bimestre/bimestral/test/crud.test.js
@@ -8,20 +8,100 @@ describe("CRUD", function() {
   describe("createItem()", function() {
     it("Adicionar um item na lista", function() {
       let c = new CRUD();
+
       c.createItem("Teste");
+
       assert.strictEqual(c.items.length, 1);
     });
 
     it("Adicionar 3 itens na lista", function() {
       let c = new CRUD();
-      c.createItem("Item 1");
-      c.createItem("Item 2");
-      c.createItem("Item 3");
-      assert.strictEqual(c.items.length, 3);
+
+        c.createItem("Item 1");
+        c.createItem("Item 2");
+        c.createItem("Item 3")
+
+        assert.strictEqual(c.items.length, 3);
     });
   });
 
-  describe("readItems()", function() {
+  describe("readItems()", function () {
+    it("Ler todos os itens", function () {
+      let c = new CRUD();
 
+        c.createItem("Item 1");
+
+        assert.strictEqual(c.items, '[Item 1]');
+    })
   });
+
+  describe("readItem()", function () {
+    it("Ler item pelo índice", function () {
+      let c = new CRUD();
+
+        c.createItem("Item 1");
+
+        assert.strictEqual(c.readItem(0), "Item 1");
+    })
+
+    it("Ler 3 itens pelo índice", function () {
+      let c = new CRUD();
+
+        c.createItem("Item 1");
+        c.createItem("Item 2");
+        c.createItem("Item 3");
+
+        assert.strictEqual(c.readItem(0), "Item 1");
+        assert.strictEqual(c.readItem(1), "Item 2");
+        assert.strictEqual(c.readItem(2), "Item 3");
+    })
+  })
+
+  describe("updateItem()", function () {
+    it("Atualiza um item", function () {
+      let c = new CRUD();
+
+        c.createItem("Item 1");
+
+        assert.strictEqual(c.updateItem(0, "Item 5"), true);
+    })
+
+    it("Atualiza dois itens", function () {
+      let c = new CRUD();
+
+      c.createItem("Item 1");
+      c.createItem("Item 2");
+
+      c.updateItem(0, "Item 2");
+      c.updateItem(1, "Item 1");
+
+      assert.strictEqual(c.readItem(0), "Item 2");
+      assert.strictEqual(c.readItem(1), "Item 1");
+    })
+  })
+
+  describe("deleteItem()", function () {
+    it("Deleta um item", function () {
+      let c = new CRUD();
+      
+      c.createItem("Teste");
+
+      c.deleteItem(0)
+
+      assert.strictEqual(c.readItem(0), undefined);
+    })
+
+    it("Deleta dois itens", function () {
+      let c = new CRUD();
+
+      c.createItem("Gabriel");
+      c.createItem("Fabricio");
+      c.createItem("Melrick");
+
+      c.deleteItem(0)
+      c.deleteItem(1)
+
+      assert.strictEqual(c.readItem(0), "Fabricio");
+    })
+  })
 });
